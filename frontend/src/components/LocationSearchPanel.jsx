92ff77a32d83f772fcd992d4075b81efdb786773
Harsh Srivastav <hsrivastav099@gmail.com>
2025-02-13 05:18:16 +0530

    LocationSearchPanel and VechilePanel done and ConfirmRide is going to start

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+    console.log(props)
+    // location search
+    const locations = [
+        "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA",
+        "221B Baker Street, London NW1 6XE, UK",
+        "1 Chome-1-2 Oshiage, Sumida City, Tokyo 131-8634, Japan",
+        "5 Avenue Anatole France, 75007 Paris, France",
+        "Sydney Opera House, Bennelong Point, Sydney NSW 2000, Australia"
+      ];
+      
+      
   return (
     <div>
-      location
+      {/*  this is just a random and temporary Location */}
+      {
+        locations.map((elem,idx) =>{
+            return (
+              <div key={idx} onClick={()=>{
+                props.setVechilePanel(true)
+                props.setPanelOpen(false)
+              }} className='flex gap-4 border-2 p-2 rounded-xl  items-center mx-2 border-gray-100 active:border-black justify-start'>
+                <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-2-fill "></i></h2>
+                <h4 className='font-medium'>{elem}</h4>
+              </div>
+            )
+        })
+      }
+
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { gsap } from 'gsap'
 import { useRef } from 'react'
 import 'remixicon/fonts/remixicon.css'
 import LocationSearchPanel from '../components/LocationSearchPanel'
+import VechilePanel from '../components/VechilePanel'
 
 const Home = () => {
   // useState
@@ -12,7 +13,9 @@ const Home = () => {
   const [pickup, setPickup] = useState('')
   const [panelOpen, setPanelOpen] = useState(false)
   const panelRef = useRef(null)
+  const vechilePanelRef = useRef(null)
   const panelCloseRef = useRef(null)
+  const [vechilePanel, setVechilePanel] = useState(false)
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -23,11 +26,10 @@ const Home = () => {
   useGSAP(() => {
     if (panelOpen) {
       gsap.to(panelRef.current, {
-        height: '70%',
+        height: '60%',
         duration: 0.5,
-        padding:20,
         ease: 'power3.inOut',
-        opacity:1
+        opacity: 1
       })
       gsap.to(panelCloseRef.current, {
         opacity: 1,
@@ -45,23 +47,38 @@ const Home = () => {
         opacity: 0,
         duration: 0.5,
         ease: 'power3.inOut',
-        
+
       })
     }
   }, [panelOpen])
 
+  //  GSAP animation
+  useGSAP(() => {
+    if (vechilePanel) {
+      gsap.to(vechilePanelRef.current, {
+        transform:'translateY(0)'
+      })
+    } else {
+      gsap.to(vechilePanelRef.current, {
+        transform:'translateY(100%)'
+      })
+    }
+  },[vechilePanel])
+
+
+
   return (
-    <div className="bg-cover bg-center h-screen ">
+    <div className="bg-cover bg-center h-screen overflow-hidden">
       <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
 
-      <div className='h-screen w-screen overflow-auto'>
+      <div  className='h-screen w-screen overflow-auto'>
         {/* image for temporary use  */}
         <img className='h-full w-full' src="https://s.wsj.net/public/resources/images/BN-XR452_201802_M_20180228165525.gif" alt="" />
       </div>
       <div className=' flex flex-col justify-end h-screen absolute top-0 w-full '>
         {/* 1 */}
-        <div className='h-[30%] p-5 bg-white '>
-          <h5 ref={panelCloseRef} onClick={()=>{
+        <div className='h-[40%] p-5 bg-white '>
+          <h5 ref={panelCloseRef} onClick={() => {
             setPanelOpen(false)
           }} className='absolute opacity-0 right-6 top-6 text-2xl'>
             <i className="ri-arrow-down-wide-line"></i>
@@ -90,14 +107,18 @@ const Home = () => {
               type="text"
               placeholder="When?" />
           </form>
-          {/* <button
+          <button
             className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
             Find Trip
-          </button> */}
+          </button>
         </div>
 
-        <div ref={panelRef} className='h-0  bg-white '>
-            <LocationSearchPanel/>
+        <div ref={panelRef} className='h-0 bg-white '>
+          <LocationSearchPanel  setPanelOpen={setPanelOpen} setVechilePanel={setVechilePanel} />
+        </div>
+
+        <div ref={vechilePanelRef} className='fixed w-full translate-y-full z-10 bottom-0 bg-white px-3 py-8'>
+              <VechilePanel setVechilePanel={setVechilePanel}/>
         </div>
       </div>
     </div>
