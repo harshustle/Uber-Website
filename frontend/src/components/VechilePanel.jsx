ab4310e81ac871033c561fb1de34ef5efc750d4a
Harsh Srivastav <hsrivastav099@gmail.com>
2025-02-27 17:15:03 +0530

    userUi created

--- frontend/src/components/VechilePanel.jsx
+++ frontend/src/components/VechilePanel.jsx
@@ -8,7 +8,9 @@ const VechilePanel = (props) => {
     }} className='p-1 w-full text-center absolute-0 top-0 text-3xl text-gray-500'> <i className="ri-arrow-down-wide-line"></i></h5>
        <h3 className='text-2xl font-semibold mb-5'>Choose a vechile</h3>
        {/* first Vechile */}
-       <div className='flex w-full p-3 border-2 mb-2 active:border-black rounded-xl items-center justify-between'>
+       <div onClick={()=>{
+         props.setConfirmRidePanel(true)
+       }} className='flex w-full p-3 border-2 mb-2 active:border-black rounded-xl items-center justify-between'>
          <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png" alt="" />
          <div className='ml-2 w-1/2'>
            <h4 className='font-medium text-base'>Uber go <span><i className="ri-user-fill"></i>4</span></h4>
@@ -18,7 +20,9 @@ const VechilePanel = (props) => {
          <h2 className='text-xl font-semibold'>$192.40</h2>
        </div>
        {/* Second Vechile */}
-       <div className='flex w-full p-3 border-2 mb-2 active:border-black rounded-xl items-center justify-between'>
+       <div onClick={()=>{
+         props.setConfirmRidePanel(true)
+       }} className='flex w-full p-3 border-2 mb-2 active:border-black rounded-xl items-center justify-between'>
          <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648177797/assets/fc/ddecaa-2eee-48fe-87f0-614aa7cee7d3/original/Uber_Moto_312x208_pixels_Mobile.png" alt="" />
          <div className='ml-2 w-1/2'>
            <h4 className='font-medium text-base'>Moto <span><i className="ri-user-fill"></i>1</span></h4>
@@ -28,7 +32,9 @@ const VechilePanel = (props) => {
          <h2 className='text-xl font-semibold'>$65.30</h2>
        </div>
        {/* Third Vechile */}
-       <div className='flex w-full p-3 border-2 mb-2 active:border-black rounded-xl items-center justify-between'>
+       <div onClick={()=>{
+         props.setConfirmRidePanel(true)
+       }} className='flex w-full p-3 border-2 mb-2 active:border-black rounded-xl items-center justify-between'>
          <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
          <div className='ml-2 w-1/2'>
            <h4 className='font-medium text-base'>Auto <span><i className="ri-user-fill"></i>3</span></h4>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,9 @@ import { useRef } from 'react'
 import 'remixicon/fonts/remixicon.css'
 import LocationSearchPanel from '../components/LocationSearchPanel'
 import VechilePanel from '../components/VechilePanel'
+import ConfirmRide from '../components/ConfirmRide'
+import LookingForDriver from '../components/LookingForDriver'
+import WaitingForDriver from '../components/WaitingForDriver'
 
 const Home = () => {
   // useState
@@ -15,7 +18,13 @@ const Home = () => {
   const panelRef = useRef(null)
   const vechilePanelRef = useRef(null)
   const panelCloseRef = useRef(null)
+  const vehicleFoundRef = useRef(null)
+  const waitingForDriverRef = useRef(null)
   const [vechilePanel, setVechilePanel] = useState(false)
+  const [confirmRidePanel, setConfirmRidePanel] = useState(false)
+  const confirmRidePanelRef = useRef(null)
+  const [vehicleFound, setVehicleFound] = useState(false)
+  const [waitingForDriver, setWaitingForDriver] = useState(false)
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -56,26 +65,64 @@ const Home = () => {
   useGSAP(() => {
     if (vechilePanel) {
       gsap.to(vechilePanelRef.current, {
-        transform:'translateY(0)'
+        transform: 'translateY(0)'
       })
     } else {
       gsap.to(vechilePanelRef.current, {
-        transform:'translateY(100%)'
+        transform: 'translateY(100%)'
       })
     }
-  },[vechilePanel])
+  }, [vechilePanel])
+
+  useGSAP(() => {
+    if (confirmRidePanel) {
+      gsap.to(confirmRidePanelRef.current, {
+        transform: 'translateY(0)'
+      })
+    } else {
+      gsap.to(confirmRidePanelRef.current, {
+        transform: 'translateY(100%)'
+      })
+    }
+  }, [confirmRidePanel])
+
+
+  useGSAP(function () {
+    if (vehicleFound) {
+      gsap.to(vehicleFoundRef.current, {
+        transform: 'translateY(0)'
+      })
+    } else {
+      gsap.to(vehicleFoundRef.current, {
+        transform: 'translateY(100%)'
+      })
+    }
+  }, [vehicleFound])
+
+  useGSAP(function () {
+    if (waitingForDriver) {
+      gsap.to(waitingForDriverRef.current, {
+        transform: 'translateY(0)'
+      })
+    } else {
+      gsap.to(waitingForDriverRef.current, {
+        transform: 'translateY(100%)'
+      })
+    }
+  }, [waitingForDriver])
+
 
 
 
   return (
-    <div className="bg-cover bg-center h-screen overflow-hidden">
+    <div className="bg-cover bg-center h-screen ">
       <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
 
-      <div  className='h-screen w-screen overflow-auto'>
+      <div className='h-screen w-screen overflow-auto'>
         {/* image for temporary use  */}
         <img className='h-full w-full' src="https://s.wsj.net/public/resources/images/BN-XR452_201802_M_20180228165525.gif" alt="" />
       </div>
-      <div className=' flex flex-col justify-end h-screen absolute top-0 w-full '>
+      <div className=' flex flex-col justify-end h-screen absolute top-0 w-full overflow-hidden'>
         {/* 1 */}
         <div className='h-[40%] p-5 bg-white '>
           <h5 ref={panelCloseRef} onClick={() => {
@@ -84,9 +131,10 @@ const Home = () => {
             <i className="ri-arrow-down-wide-line"></i>
           </h5>
           <h4 className='text-2xl font-semibold'>Find a Trip</h4>
-          <form action="" className='relative py-3' onSubmit={(e) => {
-            submitHandler(e)
-          }}>
+          <form action="" className='relative py-3'
+            onSubmit={(e) => {
+              submitHandler(e)
+            }}>
             <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
             <input
               onClick={() => {
@@ -114,11 +162,41 @@ const Home = () => {
         </div>
 
         <div ref={panelRef} className='h-0 bg-white '>
-          <LocationSearchPanel  setPanelOpen={setPanelOpen} setVechilePanel={setVechilePanel} />
+          <LocationSearchPanel
+            setPanelOpen={setPanelOpen}
+            setVechilePanel={setVechilePanel} />
         </div>
 
         <div ref={vechilePanelRef} className='fixed w-full translate-y-full z-10 bottom-0 bg-white px-3 py-8'>
-              <VechilePanel setVechilePanel={setVechilePanel}/>
+          <VechilePanel
+            setConfirmRidePanel={setConfirmRidePanel}
+            setVechilePanel={setVechilePanel} />
+        </div>
+
+        <div ref={confirmRidePanelRef} className='fixed w-full translate-y-full z-10 bottom-0 bg-white px-3 py-6'>
+          <ConfirmRide
+            setConfirmRidePanel={setConfirmRidePanel}
+            setVehicleFound={setVehicleFound}
+          />
+        </div>
+
+        <div ref={vehicleFoundRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
+          <LookingForDriver
+            setConfirmRidePanel={setConfirmRidePanel}
+            setVehicleFound={setVehicleFound} />
+        </div>
+
+        <div ref={vehicleFoundRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
+          <LookingForDriver
+            
+            setVehicleFound={setVehicleFound} />
+        </div>
+
+        <div ref={waitingForDriverRef} className='fixed w-full  z-10 bottom-0  bg-white px-3 py-6 pt-12'>
+          <WaitingForDriver
+            setVehicleFound={setVehicleFound}
+            setWaitingForDriver={setWaitingForDriver}
+          />
         </div>
       </div>
     </div>
